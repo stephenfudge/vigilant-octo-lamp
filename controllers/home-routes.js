70b6a31d31e674e9617bf356af69e350a9cadc0e
Stephen Fudge <sfudge@gmail.com>
2022-06-29 15:08:33 -0400

    signup, login working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -87,7 +87,25 @@ router.get('/country/:id', async (req, res) => {
 });
 
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
+  router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
 
 
 module.exports = router;
