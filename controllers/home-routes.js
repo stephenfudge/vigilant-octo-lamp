06170ed3906c479193310af8dc415843126f62ff
joel cronin <you@example.com>
2022-06-29 12:41:44 -0400

    FLags added and groups rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,14 +9,43 @@ const bcrypt = require('bcrypt');
 const withAuth = require('../utils/auth')
 
 
-router.get('/', async (req, res)  => {
-    try {
-        const teamData = await Teams.findAll();
-        res.status(200).json(teamData);
-    }catch(err){
-        res.status(500).json(err);
-    }
-});
+router.get('/', async (req, res) => {
+  const teamData = await Teams.findAll().catch((err) => { 
+      res.json(err);
+    });
+      const teams = teamData.map((blog) => blog.get({ plain: true }));
+      console.log('==================')
+      console.log(teams)
+      const groupA = teams.filter( group =>{
+        return group.group === 'A';
+      });
+      const groupB = teams.filter( group =>{
+        return group.group === 'B';
+      });
+      const groupC = teams.filter( group =>{
+        return group.group === 'C';
+      });
+      const groupD = teams.filter( group =>{
+        return group.group === 'D';
+      });
+      const groupE = teams.filter( group =>{
+        return group.group === 'E';
+      });
+      const groupF = teams.filter( group =>{
+        return group.group === 'F';
+      });
+      const groupG = teams.filter( group =>{
+        return group.group === 'G';
+      });
+      const groupH = teams.filter( group =>{
+        return group.group === 'H';
+      });
+      console.log(groupA)
+      res.render('homepage', { teams, groupA, groupB, groupC, groupD, groupE, groupF, groupG, groupH});
+    });
+
+
+
 
 
 module.exports = router;
